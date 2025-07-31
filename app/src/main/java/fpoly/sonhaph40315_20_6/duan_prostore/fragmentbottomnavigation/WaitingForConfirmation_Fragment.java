1177b0a8bc50151ef94c415e22b867b84935a67f
sonhoang-git <sonhaph40315@fpt.edu.vn>
2025-07-31 17:03:04 +0700

    chờ xác nhận đơn hàng khách hàng

---
+++ app/src/main/java/fpoly/sonhaph40315_20_6/duan_prostore/fragmentbottomnavigation/WaitingForConfirmation_Fragment.java
@@ -0,0 +1,22 @@
+package fpoly.sonhaph40315_20_6.duan_prostore.fragmentbottomnavigation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fpoly.sonhaph40315_20_6.duan_prostore.R;
+
+
+public class WaitingForConfirmation_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_waiting_for_confirmation_, container, false);
+    }
+}
---
+++ app/src/main/java/fpoly/sonhaph40315_20_6/duan_prostore/useractivity/OderStatus_Activity.java
@@ -0,0 +1,22 @@
+package fpoly.sonhaph40315_20_6.duan_prostore.useractivity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import fpoly.sonhaph40315_20_6.duan_prostore.R;
+
+public class OderStatus_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_oder_status);
+
+    }
+}
