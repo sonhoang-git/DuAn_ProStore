3c1984d75e3c7966f7237d50345047e4564274ef
sonhoang-git <sonhaph40315@fpt.edu.vn>
2025-07-31 20:34:15 +0700

    Thong tin , liên hệ user

--- app/src/main/java/fpoly/sonhaph40315_20_6/duan_prostore/useractivity/ProductReview_Activity.java
+++ app/src/main/java/fpoly/sonhaph40315_20_6/duan_prostore/useractivity/ProductReview_Activity.java
@@ -1,9 +1,12 @@
 package fpoly.sonhaph40315_20_6.duan_prostore.useractivity;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,10 +20,29 @@ public class ProductReview_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_product_review);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ImageView[] stars = {
+                findViewById(R.id.star1),
+                findViewById(R.id.star2),
+                findViewById(R.id.star3),
+                findViewById(R.id.star4),
+                findViewById(R.id.star5)
+        };
+
+        int yellow = ContextCompat.getColor(this, R.color.yellow);
+        int gray = ContextCompat.getColor(this, R.color.gray);
+
+        for (int i = 0; i < stars.length; i++) {
+            final int index = i;
+            stars[i].setOnClickListener(v -> {
+                for (int j = 0; j < stars.length; j++) {
+                    if (j <= index) {
+                        stars[j].setColorFilter(yellow, PorterDuff.Mode.SRC_IN);
+                    } else {
+                        stars[j].setColorFilter(gray, PorterDuff.Mode.SRC_IN);
+                    }
+                }
+            });
+
+        }
     }
 }
