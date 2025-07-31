ac1e9ed781d0d552fc0626db767e871347ed0b7b
sonhoang-git <sonhaph40315@fpt.edu.vn>
2025-07-31 20:12:01 +0700

    Đánh giá sản phẩm

---
+++ app/src/main/java/fpoly/sonhaph40315_20_6/duan_prostore/useractivity/ProductReview_Activity.java
@@ -0,0 +1,26 @@
+package fpoly.sonhaph40315_20_6.duan_prostore.useractivity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import fpoly.sonhaph40315_20_6.duan_prostore.R;
+
+public class ProductReview_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_product_review);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
